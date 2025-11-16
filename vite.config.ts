ae58d5939741b0df896b2b6899f24ec53f6ce1a1
wyclif001 <wyclifgachara@gmail.com>
2025-11-16 23:22:28 +0300

    deploying solved

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: '/ELMS_CLIENT/',  // IMPORTANT
+  base: '/'
 })
